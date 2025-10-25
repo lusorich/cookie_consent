a33fad9e47e74d8f57c0e1b95db996f1622fd188
dreamonaut <dreamonautre@gmail.com>
2025-10-25 21:03:46 +0200

    add sveltekit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script>
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+{@render children?.()}
+
+<div class="credits" data-gfe-screenshot-exclude="true">
+	A challenge by
+	<a
+		href="https://www.greatfrontend.com/projects?ref=challenges"
+		target="_blank"
+	rel="noopener noreferrer"
+	>
+		GreatFrontEnd Projects
+	</a>. Built by
+	<a
+		href="https://www.greatfrontend.com/projects/u/dreamonautre"
+		target="_blank"
+		rel="noopener noreferrer"
+	>
+		dreamonaut
+	</a>.
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<main class="h-lvh w-full bg-gradient-to-b from-gray-50 to-[#d2d6db]" >
+</main>
